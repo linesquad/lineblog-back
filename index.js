046a6b2b8260e43346546444f0bfdf4c8c19c10b
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-01-30 11:38:47 +0400

    set up server and database

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import userRouter from "./routes/user.js";
+import postRouter from "./routes/post.js";
+import commentRouter from "./routes/comment.js";
+import connectDB from "./lib/connectDB.js";
+
+const app = express();
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
+app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
